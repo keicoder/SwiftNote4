6856ddc4aab83ed2899505a67f878cd6994f9c1b
lovejun <hyun2012@gmail.com>
2014-03-03 13:49:08 +0900

    //basic tableview using storyboard with coredata from empty project
    //AppDelegate.h
    //1. 스토리보드 사용하므로 기본 스텁코드 삭제
    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
    {
        if (debug==1) {NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));}
    
        //스토리보드 사용하므로 기본 스텁코드 삭제
    
        return YES;
    }
    
    //2. 스토리보드에서 테이블 뷰 컨트롤러 추가 및 embed in navigation controller
    
    //3. 테이블 뷰 클래스 생성 및 hook up with 스토리보드 테이블 뷰
    
    //4. 테이블 뷰 기본 데이터소스 메소드 코딩

---
+++ SwiftNote4/NoteListsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoteListsTableViewController.h
+//  SwiftNote4
+//
+//  Created by jun on 2014. 3. 3..
+//  Copyright (c) 2014년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoteListsTableViewController : UITableViewController
+
+@end
---
+++ SwiftNote4/NoteListsTableViewController.m
@@ -0,0 +1,117 @@
+//
+//  NoteListsTableViewController.m
+//  SwiftNote4
+//
+//  Created by jun on 2014. 3. 3..
+//  Copyright (c) 2014년 jun. All rights reserved.
+//
+
+#import "NoteListsTableViewController.h"
+
+@interface NoteListsTableViewController ()
+
+@end
+
+@implementation NoteListsTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
