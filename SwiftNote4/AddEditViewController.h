077a786df415920f1224f7d947adfb4072ca9b3d
lovejun <hyun2012@gmail.com>
2014-03-03 14:35:04 +0900

    스토리보드 AddEditViewController 생성 및 클래스 생성 and hook up with modal segue

---
+++ SwiftNote4/AddEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddEditViewController.h
+//  SwiftNote4
+//
+//  Created by jun on 2014. 3. 3..
+//  Copyright (c) 2014년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEditViewController : UIViewController
+
+@end
---
+++ SwiftNote4/AddEditViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddEditViewController.m
+//  SwiftNote4
+//
+//  Created by jun on 2014. 3. 3..
+//  Copyright (c) 2014년 jun. All rights reserved.
+//
+
+#import "AddEditViewController.h"
+
+@interface AddEditViewController ()
+
+@end
+
+@implementation AddEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
